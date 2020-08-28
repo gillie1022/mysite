a4825b47abde4b2a2fa08d7c1dd51fb38237e7a0
gillie1022 <dave.gillie@icloud.com>
2020-08-28 11:14:08 -0400

    created base project and technology models

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    github = models.CharField(max_length=250)
+    livesite = models.CharField(max_length=250)
+
+class Technology(models.Model):
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='technologies')
+    name = models.CharField(max_length=50)
+    
