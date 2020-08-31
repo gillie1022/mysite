f55f0ff4d26bd5ada178985ad450a809fea4380c
gillie1022 <dave.gillie@icloud.com>
2020-08-31 14:42:58 -0400

    restructured models for technologies and projects

--- mysite/models.py
+++ mysite/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+class Technology(models.Model):
+    # project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='technologies')
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+        
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     github = models.CharField(max_length=250)
     livesite = models.CharField(max_length=250, null=True, blank=True)
+    technologies = models.ManyToManyField('Technology', related_name='projects')
 
     def __str__(self):
         return self.title
 
-class Technology(models.Model):
-    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='technologies')
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
